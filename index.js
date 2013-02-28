d3477659a0adb912dbc2e5610fb679e96dc2246b
Christopher Martin <ch.martin@gmail.com>
2013-02-28 03:12:57 -0500

    rainbow

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 
   $(function() {
     var svg = $('svg');
-    var catgirl = $('.catgirl');
+    var catgirl = $('#catgirl');
+    var rainbow = $('#rainbow');
+    var radialRainbow = $('#radialRainbow');
     var frame = 0;
     var loopDuration = 0.8; // second
     var frameRate = 20; // fps
@@ -12,16 +14,23 @@
     function easeIn(x) { return Math.sin(x); }
     function easeOut(x) { return Math.sin(x); }
     setInterval(function() {
+      var w = svg.width(), h = svg.height();
       var progress = frame / framesPerLoop;
       var scale = progress < biggestTime
         ? sin(progress / biggestTime)
         : 1-sin((progress - biggestTime) / (1-biggestTime));
-      var size = (2 + 2 * scale) * 0.12 * Math.min(svg.width(), svg.height());
+      var size = (2 + 2 * scale) * 0.12 * Math.min(w, h);
       catgirl.children()[1].style.opacity = Math.max(0, Math.min(1, 3*scale - 0.8));
-      var x = Math.floor((svg.width() - size) / 2);
-      var y = Math.floor((svg.height() - size) / 2);
+      var x = Math.floor((w - size) / 2);
+      var y = Math.floor((h - size) / 2);
       catgirl.attr('transform', 'translate(' + x + ',' + y + ') scale(' + size + ')'
         + ' rotate(' + (scale * -8 + 2) + ' ' + 0.5 + ' ' + 0.5 + ')');
+      var bigSide = Math.max(w, h);
+      rainbow.attr('width', bigSide);
+      rainbow.attr('height', bigSide);
+      rainbow.attr('y', w < h ? 0 : Math.floor((w-h)/-2));
+      rainbow.attr('x', h < w ? 0 : Math.floor((h-w)/-2));
+      radialRainbow.attr('r', progress * 2);
       frame = (frame + 1) % framesPerLoop;
     }, 1000 / frameRate);
   });
